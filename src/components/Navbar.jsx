f0c40f7e99625d4102a9288e8898140288e22ff1
Nihad-Canizade <nihadcanizade000@gmail.com>
2024-05-27 05:08:18 +0400

    tasty

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { CiSearch } from "react-icons/ci";
-import { CiHeart } from "react-icons/ci";
-import { FaCartPlus } from "react-icons/fa";
 
 
 function Navbar() {
@@ -10,19 +7,18 @@ function Navbar() {
     <>
       <nav className="nav">
         <div className='nav__div'>
-          <h4>Aranaz.</h4>
+          <h4>Tasty</h4>
         </div>
         <ul className='Text__ul'>
           <Link to='/' className='directior'>Home</Link>
           <Link to='/admin' className='directior'>Admin</Link>
           <Link to='/favorites' className='directior'>Favorites</Link>
-          <li>Shop</li>
-          <li>Pages</li>
+          <li>Menu</li>
+          <li>Specialties</li>
+          <li>Reservation</li>
           <li>Blog</li>
+          <li>About</li>
           <li>Contact</li>
-          <CiSearch />
-          <CiHeart />
-          <FaCartPlus />
         </ul>
       </nav>
     </>
